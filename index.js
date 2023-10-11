2fc8bbd97068b029b999cbe11c732961b3aa0aee
amine m. aouini <aminaouini565@gmail.com>
2023-10-11 22:57:32 +0100

    setup vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', async (req, res)=>{
     .sort({
         date: 'desc'
     })
-    res.render("index", {articles})
+    res.render("index", {articles}).end()
 })
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{console.log("server intiated!")})
